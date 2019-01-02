7336cdf784d0891bd62ed34c167e5972ddea4660
lshsso <45104148+lshsso@users.noreply.github.com>
2019-01-03 06:55:22 +0900

    190103
    태아이름, 임신날짜 등록

---
+++ MainView.swift
@@ -0,0 +1,40 @@
+//
+//  MainView.swift
+//  iBabyMain
+//
+//  Created by SSO on 02/01/2019.
+//  Copyright © 2019 sso. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class MainView: UIViewController {
+    
+    @IBOutlet var BabyName: UILabel!
+    
+    @IBOutlet var PWeakLbl: UILabel!
+    
+    var LabelText = String()
+    var LblText = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        BabyName.text = LabelText
+        PWeakLbl.text = LblText
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
